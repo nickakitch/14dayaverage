1b24e69cc1640beb427923341e7a902669646866
nickakitch <nicholasjkitchen@gmail.com>
2020-09-12 14:04:39 +1000

    - added google analytics

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -13,6 +13,16 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script data-ad-client="ca-pub-7562176647521925" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177882232-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-177882232-1');
+    </script>
+
     <!-- Styles -->
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
