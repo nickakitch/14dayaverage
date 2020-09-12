12c7b19ca88b35113819207cd5bb247ed7b21c5a
nickakitch <nicholasjkitchen@gmail.com>
2020-09-12 14:03:04 +1000

    - added 'data from dhhs'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
         </div>
         <div class="foot">
             <p class="text-muted small"><i class="fas fa-map-marker-alt mr-2"></i>Melbourne, VIC</p>
-            <p class="text-muted small">Updated {{ $last_updated->diffForHumans() }}</p>
+            <p class="text-muted small">Updated {{ $last_updated->diffForHumans() }} - data from <a class="text-light" href="https://www.dhhs.vic.gov.au/victorian-coronavirus-covid-19-data">dhhs.vic.gov.au</a></p>
         </div>
     </main>
 @endsection
