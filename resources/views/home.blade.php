a62a640fa0cac4bcfac7d9038165666f561404a9
nickakitch <nicholasjkitchen@gmail.com>
2020-09-13 11:08:34 +1000

    - re-added google ads

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,15 +12,5 @@
             <p class="text-muted small"><i class="fas fa-map-marker-alt mr-2"></i>Melbourne, VIC</p>
             <p class="text-muted small">Updated {{ $last_updated->diffForHumans() }} - data from <a class="text-light" href="https://www.dhhs.vic.gov.au/victorian-coronavirus-covid-19-data">dhhs.vic.gov.au</a></p>
         </div>
-        <div id="278289200">
-            <script type="text/javascript">
-                try {
-                    window._mNHandle.queue.push(function (){
-                        window._mNDetails.loadTag("278289200", "728x90", "278289200");
-                    });
-                }
-                catch (error) {}
-            </script>
-        </div>
     </main>
 @endsection
