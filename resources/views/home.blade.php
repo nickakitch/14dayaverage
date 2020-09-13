a4608b579f798864ddd93e257943c7a5b71a6329
nickakitch <nicholasjkitchen@gmail.com>
2020-09-13 10:26:33 +1000

    - removed google ads, added media ads

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,16 +12,15 @@
             <p class="text-muted small"><i class="fas fa-map-marker-alt mr-2"></i>Melbourne, VIC</p>
             <p class="text-muted small">Updated {{ $last_updated->diffForHumans() }} - data from <a class="text-light" href="https://www.dhhs.vic.gov.au/victorian-coronavirus-covid-19-data">dhhs.vic.gov.au</a></p>
         </div>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <!-- bottom-bar -->
-        <ins class="adsbygoogle"
-             style="display:block"
-             data-ad-client="ca-pub-7562176647521925"
-             data-ad-slot="6366678828"
-             data-ad-format="auto"
-             data-full-width-responsive="true"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
+        <div id="278289200">
+            <script type="text/javascript">
+                try {
+                    window._mNHandle.queue.push(function (){
+                        window._mNDetails.loadTag("278289200", "728x90", "278289200");
+                    });
+                }
+                catch (error) {}
+            </script>
+        </div>
     </main>
 @endsection
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -11,7 +11,13 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script data-ad-client="ca-pub-7562176647521925" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+    <script type="text/javascript">
+        window._mNHandle = window._mNHandle || {};
+        window._mNHandle.queue = window._mNHandle.queue || [];
+        medianet_versionId = "3121199";
+    </script>
+    <script src="https://contextual.media.net/dmedianet.js?cid=8CU642A12" async="async"></script>
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177882232-1"></script>
