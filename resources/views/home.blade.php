99c2649079381aff2e4762e47d31e1cc6d1b06a6
nickakitch <nicholasjkitchen@gmail.com>
2020-09-14 05:27:21 +1000

    - updated dhhs adjustments

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
         <div class="foot mb-2">
             <p class="text-muted small"><i class="fas fa-map-marker-alt mr-2"></i>Melbourne, VIC</p>
             <p class="text-muted small">Updated {{ $last_updated->diffForHumans() }} - data from <a class="text-light" href="https://www.dhhs.vic.gov.au/victorian-coronavirus-covid-19-data">dhhs.vic.gov.au</a></p>
-            <p>(includes dhhs adjusted data)</p>
+            <p class="text-muted small">(includes dhhs adjusted data)</p>
         </div>
     </main>
 @endsection
